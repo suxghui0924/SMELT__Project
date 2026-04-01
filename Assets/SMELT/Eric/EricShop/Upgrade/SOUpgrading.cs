caca4449d79aec87d79ac363d345878c16fc8cc9
Erica <yungeoni3794@gmail.com>
2026-04-01 18:00:15 +0900

    Revert "fix/plusskilltree"
    This reverts commit 9074754b3a715c3c2cf6cc77819a0a82099aca5b.

--- Assets/SMELT/Eric/EricShop/Upgrade/SOUpgrading.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "SOUpgrading", menuName = "Scriptable Objects/SOUpgrading")]
-public class SOUpgrading : ScriptableObject
-{
-    public string UPname;
-    public float UPtime;
-}
