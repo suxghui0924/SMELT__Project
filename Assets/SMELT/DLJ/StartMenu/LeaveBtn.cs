aec94c731326e6af9620342229a8b5e6717dade3
L0GINC0DE <l0gin.c0ded@gmail.com>
2026-04-02 23:40:33 +0900

    Made leave button

--- Assets/SMELT/DLJ/StartMenu/LeaveBtn.cs
+++ Assets/SMELT/DLJ/StartMenu/LeaveBtn.cs
@@ -4,6 +4,10 @@ public class LeaveBtn : MonoBehaviour
 {
     public void Leave()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
