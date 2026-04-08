49444a544d0c1fc27b60b2ffd17f5b67e1297ae7
suxghui0924 <dodgeair0924@gmail.com>
2026-04-08 17:16:59 +0900

    Import UI & Manager

--- Assets/SMELT/Suxghui/Scripts/Test.cs
+++ Assets/SMELT/Suxghui/Scripts/Test.cs
@@ -1,14 +1,25 @@
 using UnityEngine;
-[DefaultExecutionOrder(5)]
+[DefaultExecutionOrder(10)]
 public class Test : MonoBehaviour
 {
     [SerializeField] private Transform _gameObject;
     [SerializeField] private RectTransform _rectTransform;
     void Start()
     {
-/*        BillboardUI.Instance.AddTarget(_gameObject, _rectTransform, 0.85f);
-        Debug.Log("시작됨");*/
-        VolumeManager.instance.VolumeStart("heat", 0.25f);
+        /*        BillboardUI.Instance.AddTarget(_gameObject, _rectTransform, 0.85f);
+                Debug.Log("시작됨");*/
         //VolumeManager.instance.VolumeStart("ui",0.25f);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            VolumeManager.instance.VolumeStart("damage", "right", 0.15f);
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            VolumeManager.instance.VolumeStart("damage", "left", 0.15f);
+        }
     }
 }
--- Assets/SMELT/Suxghui/Scripts/VolumeManager.cs
+++ Assets/SMELT/Suxghui/Scripts/VolumeManager.cs
@@ -3,6 +3,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
 using System.Collections;
+using UnityEngine.Rendering.Universal;
 
 public class VolumeManager : MonoBehaviour
 {
@@ -33,35 +34,48 @@ public class VolumeManager : MonoBehaviour
         ui = m_Volumes[3].GetComponent<Volume>();
     }
 
-    public void VolumeStart(String str_name, float wait)
+    public void VolumeStart(String str_name,string dir, float wait)
     {
-        Debug.Log("작동중");
         foreach (var vol in m_Volumes)
         {
             if (vol.name == str_name.FirstCharacterToUpper() + "_Volume")
             {
-                StartCoroutine(StartVolume(vol, wait));
+                StartCoroutine(StartVolume(vol, dir, wait));
             }
         }
     }
 
-    IEnumerator StartVolume(Volume vol, float wait)
+    IEnumerator StartVolume(Volume vol, string dir, float wait)
     {
-        yield return StartCoroutine(VolumeSet(vol, 0, 1.0f, wait));
-        yield return StartCoroutine(VolumeSet(vol, 1.0f, 0, wait));
+        yield return StartCoroutine(VolumeSet(vol, dir, 0, 1.0f, wait));
+        yield return StartCoroutine(VolumeSet(vol, dir, 1.0f, 0, wait));
     }
-    IEnumerator VolumeSet(Volume vol, float start, float end, float wait)
+    IEnumerator VolumeSet(Volume vol, string dir, float start, float end, float wait)
     {
         float dur = wait;
+        float lens_Dur = wait / 5f;
         float time = 0;
-        while (dur > time)
+        float startX = 0.5f;
+        float targetX = 0.5f;
+        if (vol.profile.TryGet<LensDistortion>(out var lens))
         {
-            time += Time.deltaTime;
-
-            vol.weight = Mathf.Lerp(start, end, time / dur);
-            //vol.weight = Mathf.Lerp(start, end, time / dur * time / dur * (3f - 2f * time / dur));
-
-            yield return null;
+            if (dir == "left")
+            {
+                startX = (startX == 0) ? .5f: 0;
+                targetX = (targetX == 0) ? 0 : .5f;
+            }
+            else
+            {
+                startX = (startX == 0) ? .5f : 1f;
+                targetX = (targetX == 0) ? 1f : .5f;
+            }
+            while (dur > time)
+            {
+                time += Time.deltaTime;
+                vol.weight = Mathf.Lerp(start, end, time / dur);
+                lens.center.value = new Vector2(Mathf.Lerp(startX, targetX, time / dur) , 0f);
+                yield return null;
+            }
         }
     }
 }
