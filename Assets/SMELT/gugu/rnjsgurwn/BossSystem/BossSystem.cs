4b67c8b803bcfc48a1e3ee4ea1af932eafd6ef7b
kgurwn2010-cloud <kgurwn2010@gmail.com>
2026-04-28 23:41:21 +0900

    feat

--- Assets/SMELT/gugu/rnjsgurwn/BossSystem/BossSystem.cs
+++ Assets/SMELT/gugu/rnjsgurwn/BossSystem/BossSystem.cs
@@ -1,6 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossSystem : MonoBehaviour
 {
-    //플레이어랑 충돌하면 체력 감소, bosshp에서 instance로 해서 만들어야 할듯요
+    public Image _Hp;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            _Hp.fillAmount -= 0.1f;
+            Debug.Log("플레이어와 충돌함");
+           
+
+        }
+    }
 }
