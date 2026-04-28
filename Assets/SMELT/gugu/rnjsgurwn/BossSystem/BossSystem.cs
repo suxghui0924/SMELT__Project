c51981fea1f9fda3170e82b07d80891525966990
kgurwn2010-cloud <kgurwn2010@gmail.com>
2026-04-29 00:10:49 +0900

    feat

--- Assets/SMELT/gugu/rnjsgurwn/BossSystem/BossSystem.cs
+++ Assets/SMELT/gugu/rnjsgurwn/BossSystem/BossSystem.cs
@@ -4,13 +4,35 @@ using UnityEngine.UI;
 public class BossSystem : MonoBehaviour
 {
     public Image _Hp;
+    public static BossSystem Instance;
+    private float Damage = 10f; //근데 이건 곡괭이에따라서 데미지 달라져서 수정헤야함
+    private int per = 0;
+
+    
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void Init(float damage, int per)
+    {
+        this.Damage = damage;
+        this.per = per;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            _Hp.fillAmount -= 0.1f;
+            
             Debug.Log("플레이어와 충돌함");
-           
+            // 보스 max hp에서 damage만큼 감소시키는 로직 추가
+            _Hp.fillAmount -= Damage / 100f; 
+             if (_Hp.fillAmount <= 0)
+             {
+                 Debug.Log("보스가 죽었습니다!");
+                
+            }
+
 
         }
     }
