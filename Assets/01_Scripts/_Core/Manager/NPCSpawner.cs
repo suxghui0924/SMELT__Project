24740b37663be9e489ce6f55d4548dce49d13526
suxghui0924 <dodgeair0924@gmail.com>
2026-05-13 22:25:22 +0900

    Import NPCMovement & Edit House.Scene

---
+++ Assets/01_Scripts/_Core/Manager/NPCSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace _01_Scripts.Player.Manager
+{
+    public class NPCSpawner : MonoBehaviour
+    {
+        [SerializeField] public GameObject npcPrefab;
+
+        private void Awake()
+        {
+            
+        }
+    }
+}
