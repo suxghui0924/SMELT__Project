38485de286f48c6f228dd63be47663b350ac5e2c
suxghui0924 <dodgeair0924@gmail.com>
2026-05-14 08:43:46 +0900

    Import Asset SPUM & Edit NPC Systems

--- Assets/01_Scripts/_Core/Manager/NPCSpawner.cs
+++ Assets/01_Scripts/_Core/Manager/NPCSpawner.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using _01_Scripts.NPC.Data;
 using UnityEngine;
 
 namespace _01_Scripts.Player.Manager
 {
     public class NPCSpawner : MonoBehaviour
     {
-        [SerializeField] public GameObject npcPrefab;
-
+        public NPCListDataSO npcPrefab;
+        public Transform StartPos;
+        public Transform TurnPos;
+        public Transform LastPos;
         private void Awake()
         {
             
