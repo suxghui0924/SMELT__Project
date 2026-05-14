4813533ad913b2e3b194acf6840a104030915892
kgurwn2010-cloud <kgurwn2010@gmail.com>
2026-05-14 23:21:40 +0900

    feat
    HelpHelpHelp

--- Assets/SMELT/gugu/Script/BossSystem/pressjuice.cs
+++ Assets/SMELT/gugu/Script/BossSystem/pressjuice.cs
@@ -8,7 +8,7 @@ public class pressjuice : MonoBehaviour
     [SerializeField] private float spawnDelay = 7f; // 생성 쿨타임 (변경 가능+ 나중에 스킬 추가 되면 지우고 랜덤으로 바꿀예정)
     [SerializeField] private int spawnCount = 1;// 생성 개수 (변경 가능)
 
-    private float timer = 0f;
+   
 
 
    public void SpawnJuice()
