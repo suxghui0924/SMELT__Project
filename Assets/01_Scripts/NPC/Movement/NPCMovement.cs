7d81f73dd86be1e05059076052117fe182b1f4a8
Erica <yungeoni3794@gmail.com>
2026-05-17 03:30:34 +0900

    Fix base conflicts using Erici/Up files

--- Assets/01_Scripts/NPC/Movement/NPCMovement.cs
+++ Assets/01_Scripts/NPC/Movement/NPCMovement.cs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 ﻿    using System;
-=======
-﻿using System;
->>>>>>> parent of 33da136 (Revert "Reapply "Merge branch 'base' into Eric/Upgrade"")
 using _01_Scripts.Player.Manager;
 using DG.Tweening;
 using Unity.VisualScripting;
@@ -15,12 +11,8 @@ namespace _01_Scripts.NPC
     {
         [SerializeField] private float _timer;
         [SerializeField] private float _offset;
-<<<<<<< HEAD
         
         public int Index { get; private set; }
-=======
-        public int _index { get; private set; }
->>>>>>> parent of 33da136 (Revert "Reapply "Merge branch 'base' into Eric/Upgrade"")
 
         private Transform _startPos;
         private Transform _turnPos;
@@ -30,7 +22,6 @@ namespace _01_Scripts.NPC
         private bool _isTurn;
         private bool _isTurned;
         private bool _checkLastPos;
-<<<<<<< HEAD
 
         private Tween _moveTweener;
         private Sequence _exitSequence;
@@ -45,28 +36,6 @@ namespace _01_Scripts.NPC
                 _turnPos = _spawner.TurnPos;
                 _lastPos = _spawner.LastPos;  
             }
-=======
-        
-        private Sequence mySequence;
-
-        private NPCSpawner NPCSpawner;
-        
-        private void Awake()
-        {
-             
-        }
-
-        private void OnEnable()
-        {
-            NPCSpawner = gameObject.GetComponentInParent<NPCSpawner>();
-         
-            _startPos = NPCSpawner.StartPos;
-            _turnPos = NPCSpawner.TurnPos;
-            _lastPos = NPCSpawner.LastPos;  
-            _index = 3;
-            transform.DOKill();
-            Move();
->>>>>>> parent of 33da136 (Revert "Reapply "Merge branch 'base' into Eric/Upgrade"")
         }
         
         #region test
@@ -82,7 +51,6 @@ namespace _01_Scripts.NPC
 
         #endregion
 
-<<<<<<< HEAD
         public void IndexChange(int newIndex)
         {
             Index = newIndex;
@@ -118,80 +86,6 @@ namespace _01_Scripts.NPC
         private void OnDisable()
         {
             KillAllTweens();
-=======
-        public void IndexChange(int index)
-        {
-            _index = --index;
-            Move();
-        }
-        #region NPCMovement
-        private void Move()
-        {
-            if ( _index == 0 || _checkLastPos)
-            {
-                Turn();
-            }
-            else
-            {
-                if (!_isMoving)
-                {
-                    this.gameObject.transform.position = _startPos.position;
-                    _isMoving = true;
-                    mySequence = DOTween.Sequence();
-                    mySequence.Append(transform.DOMove(_lastPos.position + ((Vector3.down * _index * _offset)),
-                        _timer));
-                    if(_index == 1)
-                        mySequence.OnComplete(() => _checkLastPos = true);
-                }
-                else if (_isTurn || _isTurned)
-                {
-                    if (_isTurned)
-                        Turned();
-                    if (_isTurn)
-                        Turn();
-                }
-                else
-                {
-                    mySequence = DOTween.Sequence();
-                    mySequence.Append(transform.DOMove(_lastPos.position + ((Vector3.down * _index * _offset)),
-                        _timer));
-                    if(_index == 1)
-                        mySequence.OnComplete(() => _checkLastPos = true);
-                }
-            }
-        }
-
-        private void Turn()
-        {
-            _isTurn = true;
-            if (_isTurn && !_isTurned)
-            {
-                mySequence = DOTween.Sequence();
-                mySequence.Append(transform.DOMove(_turnPos.position, _timer / 2));
-                mySequence.OnComplete(Turned);
-            }
-            else
-            {
-                Turned();
-            }
-        }
-
-        private void Turned()
-        {
-            _isTurned = true;
-            if (_isTurned)
-            {
-                mySequence = DOTween.Sequence();
-                mySequence.Append(transform.DOMoveY(_startPos.position.y, _timer / 2));
-            }
-        }
-        #endregion
-        private void OnDisable()
-        {
-            if (mySequence == null) return;
-            mySequence.Kill();
-            mySequence = null;
->>>>>>> parent of 33da136 (Revert "Reapply "Merge branch 'base' into Eric/Upgrade"")
         }
     }
 }
