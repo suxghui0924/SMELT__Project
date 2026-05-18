42243b307350fd79861ec346dd81243586ca4c12
suxghui0924 <dodgeair0924@gmail.com>
2026-05-18 23:21:18 +0900

    Revert "Merge branch 'feat/suxghui' into base"
    This reverts commit 74572dec8bc9d26f323a44d23988c9e71be9aaf1, reversing
    changes made to a3698fce50e88d45770edb38d0d52604244fb88f.

--- Assets/SMELT/DLJ/StartMenu/Achievement/Script/AchievementTab.cs
+++ Assets/SMELT/DLJ/StartMenu/Achievement/Script/AchievementTab.cs
@@ -5,14 +5,12 @@ public class AchievementTab : MonoBehaviour
 {
     [SerializeField] private GameObject AchievementCanvas;
     public bool AchievementCanvasState = false;
-    private GameObject AchievementCanvasObject;
+
     private void Update()
     {
         foreach (var achievement in AchievementClear.instance.achievementSOs)
         {
-            AchievementCanvasObject = AchievementClear.instance.AchievementDark[achievement.achievementID];
-            if(AchievementCanvasObject != null)
-                AchievementCanvasObject.SetActive(!achievement.clear);
+            AchievementClear.instance.AchievementDark[achievement.achievementID].SetActive(!achievement.clear);
         }
     }
     public void ShowAchievement()
