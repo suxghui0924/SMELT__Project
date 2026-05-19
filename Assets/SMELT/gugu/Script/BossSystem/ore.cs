217619a1974a73dc2600bf200e55597101a125f5
kgurwn2010-cloud <kgurwn2010@gmail.com>
2026-05-20 00:32:40 +0900

    feat/carrot

--- Assets/SMELT/gugu/Script/BossSystem/ore.cs
+++ Assets/SMELT/gugu/Script/BossSystem/ore.cs
@@ -1,24 +1,55 @@
+using System;
 using UnityEngine;
 
 public class ore : MonoBehaviour
 {
-   
+    public bool canHitBoss = false;
     private Transform playerTrm;
+    private Transform bossTrm;
 
     private Rigidbody2D rb;
 
     private float speed = 3f;
-    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Paring")
+        {
+            canHitBoss = true;
+        }
+    }
 
     private void Awake()
     {
-        playerTrm = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        GameObject boss = GameObject.FindGameObjectWithTag("Boss");
+
+        
+        playerTrm = GameObject.FindGameObjectWithTag("Player").transform;
+        bossTrm = GameObject.FindGameObjectWithTag("Boss").transform;
+
+    }
+
+    
+
     private void FixedUpdate()
     {
-        Vector2 dir = (playerTrm.position - transform.position).normalized;
-        rb.linearVelocity = dir * speed;
+        if (canHitBoss)
+        {
+            Vector2 direction = bossTrm.position - transform.position;
+            rb.linearVelocity = direction * speed;
+
+        }
+        else
+        {
+            Vector2 dir = (playerTrm.position - transform.position).normalized;
+            rb.linearVelocity = dir * speed;
+        }
+        
+        
     }
 }
