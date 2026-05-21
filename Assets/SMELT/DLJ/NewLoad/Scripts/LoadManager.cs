c0872714866f8d490b1bf7ef4677fa8e07c5eb82
Erica <yungeoni3794@gmail.com>
2026-05-21 19:08:38 +0900

    fix/loadmanager/Use/Async

--- Assets/SMELT/DLJ/NewLoad/Scripts/LoadManager.cs
+++ Assets/SMELT/DLJ/NewLoad/Scripts/LoadManager.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class LoadManager : MonoBehaviour
 {
+    [SerializeField] private float minLoadTime;
+
     private void Start()
     {
         StartCoroutine(LoadAsyncScene());
     }
+
     IEnumerator LoadAsyncScene()
     {
         // 매니저에 저장된 다음 씬 이름을 비동기로 로드
         AsyncOperation op = SceneManager.LoadSceneAsync(SceneLoader.nextScene);
         op.allowSceneActivation = false; // 100% 로드되어도 바로 넘어가지 않게 방지
+        while (!op.isDone)
+        {
+            yield return null;
+            minLoadTime += Time.unscaledDeltaTime;
+            Debug.Log(minLoadTime +" " + op.progress);
 
-        yield return new WaitForSeconds(3f);
-        
-        UICanvasManager.instance.FadeStart();
-        op.allowSceneActivation = true;
-        switch(SceneLoader.nextScene.ToString())
+            if (op.progress >= 0.9f && minLoadTime >= 1f)
+            {
+                UICanvasManager.instance.FadeStart();
+                op.allowSceneActivation = true;
+            }
+        }
+
+        switch (SceneLoader.nextScene)
         {
             case "House":
                 UICanvasManager.instance.SetCanvasActive(CanvasType.Hud, true);
