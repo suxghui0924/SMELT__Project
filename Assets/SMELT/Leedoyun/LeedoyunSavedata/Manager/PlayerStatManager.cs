f1d40e69b3e95fb8ac34c0dcec61104cd8e39923
Erica <yungeoni3794@gmail.com>
2026-05-23 18:34:32 +0900

    fix/stat

--- Assets/SMELT/Leedoyun/LeedoyunSavedata/Manager/PlayerStatManager.cs
+++ Assets/SMELT/Leedoyun/LeedoyunSavedata/Manager/PlayerStatManager.cs
@@ -113,6 +113,7 @@ public class PlayerStatManager : MonoBehaviour, ISaveable
         switch (id)
         {
             case "AllUp1":
+                UpGetFruits += times;
                  break;
             case "AllUp2":
                 UpGetFruits += times;
