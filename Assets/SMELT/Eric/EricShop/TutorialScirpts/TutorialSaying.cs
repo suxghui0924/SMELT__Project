e681b1c00ba9190560ae836ca21fc892e39a6e6a
Erica <yungeoni3794@gmail.com>
2026-05-23 21:32:05 +0900

    fix/tutorial/when/dungeon/attack

--- Assets/SMELT/Eric/EricShop/TutorialScirpts/TutorialSaying.cs
+++ Assets/SMELT/Eric/EricShop/TutorialScirpts/TutorialSaying.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TutorialSaying : MonoBehaviour
 {
@@ -10,18 +11,43 @@ public class TutorialSaying : MonoBehaviour
         [SerializeField] private string characterName;
         [SerializeField] private float delayBetweenLines;
         [SerializeField] private string[] lines;
+        [SerializeField] private bool attack;
 
         private void Start()
         {
-                StartCoroutine(SayingCoroutine(TutorialLine.Instance.enterDungeon1));
+                StartCoroutine(SayingCoroutine(TutorialLine.Instance.dungeon3));
         }
 
         public IEnumerator SayingCoroutine(string[] texts)
         {
+                dialogue.canMove = false;
+      
                 yield return new WaitForSecondsRealtime(0.5f);
-                Time.timeScale = 0f;
+                Time.timeScale = 0f;        
+                if(TutorialLine.Instance.dungeon3 == texts)
+                {
+                        attack = true;
+                }
                 lines = texts;
                 dialogue.typingSpeed = typingSpeed;
                 dialogue.Say(lines, characterName, delayBetweenLines);
+        }
+
+        private void Update()
+        {
+                if (Input.GetMouseButtonDown(0))
+                {
+                        dialogue.Skip();
+                }
+
+                if(attack)
+                {
+                        if (Keyboard.current.dKey.wasPressedThisFrame)
+                        {
+                                dialogue.canMove = true;
+                                attack = false;
+                                dialogue.Skip();
+                        }
+                }
         }
 }
