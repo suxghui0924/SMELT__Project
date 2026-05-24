fc2e1d0be07b53d8738e4946bf72eb0b8373c440
suxghui0924 <dodgeair0924@gmail.com>
2026-05-24 20:45:59 +0900

    Added PortalSpawner.cs Edit NextDay.cs PrototypeHUD.cs PrototypeZone.cs & Update House Scene

---
+++ Assets/01_Scripts/_Core/Portal/PortalSpawner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace _01_Scripts.Player.Portal
+{
+    public class PortalSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _portalObject;
+        private void OnEnable()
+        {
+            InventoryManager.Instance.OnDayChanged += HandleDayChanged;
+        }
+
+        private void OnDisable()
+        {
+            InventoryManager.Instance.OnDayChanged -= HandleDayChanged;
+        }
+        
+        private void HandleDayChanged(int arg1, ulong arg2)
+        {
+            if (arg1 >= 7)
+            {
+                _portalObject.SetActive(true);
+            }
+        }
+    }
+}
