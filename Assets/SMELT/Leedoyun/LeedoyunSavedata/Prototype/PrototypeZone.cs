d2274960797f6dd2d5bb8ec62fbb00ba26f8e08a
suxghui0924 <dodgeair0924@gmail.com>
2026-05-25 01:25:48 +0900

    Added Collect Sound PrototypeZone.cs & Update House Scene

--- Assets/SMELT/Leedoyun/LeedoyunSavedata/Prototype/PrototypeZone.cs
+++ Assets/SMELT/Leedoyun/LeedoyunSavedata/Prototype/PrototypeZone.cs
@@ -203,7 +203,7 @@ public class PrototypeZone : MonoBehaviour
         if (ZoneType == ZoneType.StoreBroken)
         {
             if (GameManager.instance != null)
-                GameManager.instance.ChangeState(new BrokenState());
+                GameManager.instance.ChangeState(new GameOverState());
         }
     }
 }
